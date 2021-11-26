ae1f9cb9501b2054b8f442493ac1b55c39002332
Dragana <dragana.bogicevic83@gmail.com>
2021-11-26 12:40:42 +0100

    changed http to https in endpoints

--- src/service/service.ts
+++ src/service/service.ts
@@ -2,22 +2,22 @@ import axios from "axios";
 import { ShowDataTypes, ShowDataType, ShowCrewType } from "../types/types";
 
 const apiClient = axios.create({
-  baseURL: "http://api.tvmaze.com/shows",
+  baseURL: "https://api.tvmaze.com/shows",
   
 });
 
 const findAll = async () => {
-  const response = await apiClient.get<ShowDataTypes[]>("http://api.tvmaze.com/shows");
+  const response = await apiClient.get<ShowDataTypes[]>("https://api.tvmaze.com/shows");
   return response.data;
 }
 
 const findSelected = async (path: string) => {
-  const response = await apiClient.get<ShowDataType>(`http://api.tvmaze.com/shows${path}`);
+  const response = await apiClient.get<ShowDataType>(`https://api.tvmaze.com/shows${path}`);
   return response.data;
 }
 
 const findSelectedCrew = async (path: string) => {
-  const response = await apiClient.get<ShowCrewType[]>(`http://api.tvmaze.com/shows${path}`);
+  const response = await apiClient.get<ShowCrewType[]>(`https://api.tvmaze.com/shows${path}`);
   return response.data;
 }
 
